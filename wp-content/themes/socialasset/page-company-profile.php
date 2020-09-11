3e5b64d0cd91f1e0d32a64c5c9facec2aa20834f
Rannojit <cserannojitsarkar22@gmail.com>
2020-09-11 21:19:30 +0600

    .

---
+++ wp-content/themes/socialasset/page-company-profile.php
@@ -0,0 +1,83 @@
+<?php
+/*
+  Template Name: Company Profile
+*/
+get_header();
+
+?>
+
+<section class="s2-page-bnr-cntlr inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/page-bnr-company-profile.jpg);">
+	<div class="s2-page-bnr-con">
+		<h1 class="s2-page-bnr-title">Accenture Greece</h1>
+	</div>
+
+	<div class="bnr-circle">
+		<img src="<?php echo THEME_URI;?>/assets/images/company-profile-brn-logo-01.png">
+	</div>
+
+</section>
+
+
+<section class="company-profile-page-con-top">
+	<div class="container">
+		<div class="row"> 
+			<div class="col-sm-12">
+				<div class="cppct-des-cntrl">
+					<div class="cppct-des">
+						<div class="cppct-des-inr">
+							<p>Accenture is a leading global professional services company, providing services and solutions in strategy, consulting, digital, technology and operations.</p>
+							<img src="<?php echo THEME_URI;?>/assets/images/cppct-des-img-01.jpg">
+							<p>Combining unmatched experience and specialized skills across more than 40 industries and all business functions—underpinned by the world’s largest delivery network—Accenture works at the intersection of business and technology to help clients improve their performance and create sustainable value for their stakeholders. With more than 450,000 people serving clients in over 120 countries, Accenture drives innovation to improve the way the world works and lives. Visit us at </p>
+						</div>
+					</div>
+					<div class="supported-ngos-area">
+						<ul class="reset-list ulc">
+							<li>
+								<div class="supported-ngo-item">
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/sa-cocacola.png">
+									</div>
+									<strong>NGO Title</strong>
+								</div>
+							</li>
+							<li>
+								<div class="supported-ngo-item">
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/sa-cocacola.png">
+									</div>
+									<strong>NGO Title</strong>
+								</div>
+							</li>
+							<li>
+								<div class="supported-ngo-item">
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/sa-cocacola.png">
+									</div>
+									<strong>NGO Title</strong>
+								</div>
+							</li>
+							<li>
+								<div class="supported-ngo-item">
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/sa-cocacola.png">
+									</div>
+									<strong>NGO Title</strong>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</section>
+
+
+
+
+
+<?php
+get_footer();
+
+?>

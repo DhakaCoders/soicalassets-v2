b1af29b31f08c59adfb17d0da7bff8080a6c0382
Rannojit <cserannojitsarkar22@gmail.com>
2020-09-12 22:50:50 +0600

    .

---
+++ wp-content/themes/socialasset/page-for-businesses.php
@@ -0,0 +1,140 @@
+<?php
+/*
+  Template Name: For Businesses
+*/
+get_header();
+
+?>
+
+<section class="page-bnr-itb-cntlr inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/for-bsns-bg.png);">
+	<div class="page-bnr-itb-con">
+		<div class="page-bnr-itb-icon">
+			<img src="<?php echo THEME_URI;?>/assets/images/page-bnr-itb-icon-01.png">
+		</div>
+		<h1 class="page-bnr-itb-title">For Businesses</h1>
+		<a class="pbitb-btn" href="#">CREATE ACCOUNT</a>
+	</div>
+</section>
+
+
+<section class="s2-for-businesses-page-cntlr">
+	<div class="s2-to-be-supported-page-entry-hdr">
+		<span class="s2tbspeh-top-bg"></span>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="s2tbspeh-con-block">
+						<div class="s2tbspeh-des">
+							<p>Social Asset was founded with Solving Social Problems in mind. 
+							Our vision is to build a seamless platform that is easy and transparent, for both NGOs and Businesses to transact. We envision Social Asset as the go-to place for all lore ipsum</p>
+							<div class="video-play">
+				                <a data-fancybox href="https://youtu.be/_Nua3Cjdik0">
+				                  <img alt="" src="<?php echo THEME_URI;?>/assets/images/page-video-img-2.jpg">
+				                </a>
+			              	</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="sa-testimonial-section">
+	  <div class="container-xs">
+	    <div class="row">
+	      <div class="col-sm-12">
+	        <div class="sa-testimonial-sec-top">
+	          <h4>Testimonials</h4>
+	          <p>Here’re what our clients have to say about our services</p>
+	        </div>
+	        <div class="sa-testimonial-section-wrap">
+	          <div class="sa-testimonial-slider">
+	            <div class="sa-testimonial-section-inner">
+	              <div class="sa-testimonial-sec-profile">
+	                <img src="<?php echo THEME_URI;?>/assets/images/sa-testimonial-profile.png">
+	              </div>
+	              <div class="sa-testimonial-sec-blockquot">
+	                <div class="sa-testimonial-sec-blockquot-inner">
+	                  <p><strong>MYMENTOR</strong> makes me feel confident about my Qualifications and my daly performance in my job. I suggest to all women who work in STEAM domain to try this product.</p>
+	                  <h6>Mary M.</h6>
+	                  <a href="#">Junior Developer</a>
+	                </div>
+	              </div>
+	            </div>
+	            <div class="sa-testimonial-section-inner">
+	              <div class="sa-testimonial-sec-profile">
+	                <img src="<?php echo THEME_URI;?>/assets/images/sa-testimonial-profile.png">
+	              </div>
+	              <div class="sa-testimonial-sec-blockquot">
+	                <div class="sa-testimonial-sec-blockquot-inner">
+	                  <p><strong>MYMENTOR</strong> makes me feel confident about my Qualifications and my daly performance in my job. I suggest to all women who work in STEAM domain to try this product.</p>
+	                  <h6>Mary M.</h6>
+	                  <a href="#">Junior Developer</a>
+	                </div>
+	              </div>
+	            </div>
+	            <div class="sa-testimonial-section-inner">
+	              <div class="sa-testimonial-sec-profile">
+	                <img src="<?php echo THEME_URI;?>/assets/images/sa-testimonial-profile.png">
+	              </div>
+	              <div class="sa-testimonial-sec-blockquot">
+	                <div class="sa-testimonial-sec-blockquot-inner">
+	                  <p><strong>MYMENTOR</strong> makes me feel confident about my Qualifications and my daly performance in my job. I suggest to all women who work in STEAM domain to try this product.</p>
+	                  <h6>Mary M.</h6>
+	                  <a href="#">Junior Developer</a>
+	                </div>
+	              </div>
+	            </div>
+	          </div>
+	        </div>
+	        <div class="sa-testimonial-icon">
+	          <img src="<?php echo THEME_URI;?>/assets/images/sa-testimonial-icon.png">
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div><!-- end of sa-testimonial-section -->
+
+
+	<div class="sa-companies-section">
+	  <div class="container">
+	    <div class="row">
+	      <div class="col-sm-12">
+	        <div class="sa-companies-sec-inner">
+	          <h4>Companies that use Social Assets</h4>
+	          <div class="sa-company-name">
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-nokia.png"></i>
+	            </div>
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-cocacola.png"></i>
+	            </div>
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-accenture.png"></i>
+	            </div>
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-nestle.png"></i>
+	            </div>
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-nokia.png"></i>
+	            </div>
+	            <div class="sa-copany-name-slider-item">
+	              <i><img src="<?php echo THEME_URI;?>/assets/images/sa-accenture.png"></i>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	</div><!-- end of sa-companies-section -->
+
+</section>
+
+
+
+
+
+<?php
+get_footer();
+
+?>

1b52cb47741656fc817160c87f4ac7e8403b19f0
Rannojit <cserannojitsarkar22@gmail.com>
2020-09-12 22:22:16 +0600

    .

--- wp-content/themes/socialasset/page-to-be-supported-ngo.php
+++ wp-content/themes/socialasset/page-to-be-supported-ngo.php
@@ -6,24 +6,173 @@ get_header();
 
 ?>
 
-<section class="s2-page-bnr-cntlr inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/page-bnr-NGO-profile.jpg);">
-	<div class="s2-page-bnr-con">
-		<h1 class="s2-page-bnr-title">ActionAid Hellas</h1>
+<section class="page-bnr-itb-cntlr inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/for-bsns-bg.png);">
+	<div class="page-bnr-itb-con">
+		<div class="page-bnr-itb-icon">
+			<img src="<?php echo THEME_URI;?>/assets/images/page-bnr-itb-icon-01.png">
+		</div>
+		<h1 class="page-bnr-itb-title">ActionAid Hellas</h1>
+		<a class="pbitb-btn" href="#">CREATE ACCOUNT</a>
 	</div>
-
-	<div class="bnr-circle">
-		<img src="<?php echo THEME_URI;?>/assets/images/NGO-profile-brn-logo-01.png">
-	</div>
-
 </section>
 
 
 <section class="s2-to-be-supported-page-cntlr">
-	<div class="container">
-		<div class="row"> 
-			<div class="col-sm-12">
-				<div class="s2-ngo-profile-block">
-					
+	<div class="s2-to-be-supported-page-entry-hdr">
+		<span class="s2tbspeh-top-bg"></span>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="s2tbspeh-con-block">
+						<div class="s2tbspeh-des">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras et mi eros. Curabitur scelerisque, nulla non porta consectetur, odio arcu placerat neque, non euismod elit tellus sed ipsum. Cras ultrices lorem non augue pulvinar mattis. Vestibulum quis dignissim augue. Sed condimentum enim id ex pharetra, sit amet bibendum risus convallis. Phasellus sed pretium leo, sed congue nisl. Pellentesque dapibus eros justo, eget faucibus nisi dictum in. Etiam ac mollis justo, ut scelerisque lacus. Nunc ac sem id nulla .</p>
+							<img src="<?php echo THEME_URI;?>/assets/images/s2tbspeh-img.jpg">
+							<a class="pbitb-btn" href="#">CREATE ACCOUNT</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="collaborating-ngos-area-sec">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="collaborating-ngos-area-sec-inr">
+						<div class="cnas-lft-icon">
+							<img src="<?php echo THEME_URI;?>/assets/images/cnas-lft-icon.png">
+						</div>
+						<div style="position: relative; z-index: 2;">
+							<h3 class="cnas-title">Συνεργαζόμενες ΜΚΟ</h3>
+							<ul class="ulc">
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/collaborating-ngo-icon-01.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/collaborating-ngo-icon-02.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/collaborating-ngo-icon-03.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/collaborating-ngo-icon-04.png">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="thay-support-us">
+		<div class="container">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="block-1000">
+							<h3 class="tsu-title">They Support us</h3>
+							<ul class="ulc">
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-2.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-3.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-1.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-3.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-1.png">
+									</div>
+								</li>
+								<li>
+									<div>
+										<img src="<?php echo THEME_URI;?>/assets/images/partner-4.png">
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="press-section">
+		<div class="container">
+			<div class="row"> 
+				<div class="col-sm-12">
+					<div class="press-sec-hdr">
+						<h3 class="press-sec-hdr-title">Press</h3>
+						<p>Lorem ipsum dolor sit amen</p>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="press-sec-grds">
+						<ul class="ulc clearfix">
+							<li>
+								<div class="press-sec-grd-item">
+									<div class="press-sec-grd-item-fea-img">
+										<a class="overlay-link" href="#"></a>
+										<div class="inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/press-sec-grd-item-fea-img-01.jpg);"></div>
+									</div>
+									<div class="press-sec-grd-item-des mHc">
+										<h5 class="psgi-title">Writing in the Sciences</h5>
+										<p>This course teaches scientists to become more effective writer. Topics include: principles of good writing,  the format of a scientific manuscript, ethical issues in scientific publications.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="press-sec-grd-item">
+									<div class="press-sec-grd-item-fea-img">
+										<a class="overlay-link" href="#"></a>
+										<div class="inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/press-sec-grd-item-fea-img-02.jpg);"></div>
+									</div>
+									<div class="press-sec-grd-item-des mHc">
+										<h5 class="psgi-title">Introduction to Translational Science</h5>
+										<p>Translational science seeks to speed up the process of moving research discoveries from the laboratory into healthcare practices.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="press-sec-grd-item">
+									<div class="press-sec-grd-item-fea-img">
+										<a class="overlay-link" href="#"></a>
+										<div class="inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/press-sec-grd-item-fea-img-03.jpg);"></div>
+									</div>
+									<div class="press-sec-grd-item-des mHc">
+										<h5 class="psgi-title">Open Source tools for Data Science</h5>
+										<p>In this course, you’ll learn about Jupyter Notebooks, RStudio IDE, Apache Zeppelin and Data Science Experience.</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+						<div>
+							<a class="pbitb-btn" href="#">VIEW ALL ARTICLES</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>

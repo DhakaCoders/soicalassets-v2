529fccb3f98fc829cbf98c7f1e59a56b6ec5a5b7
Rannojit <cserannojitsarkar22@gmail.com>
2020-09-12 20:52:15 +0600

    .

---
+++ wp-content/themes/socialasset/page-to-be-supported-ngo.php
@@ -0,0 +1,40 @@
+<?php
+/*
+  Template Name: To Be Supported NGO
+*/
+get_header();
+
+?>
+
+<section class="s2-page-bnr-cntlr inline-bg" style="background: url(<?php echo THEME_URI;?>/assets/images/page-bnr-NGO-profile.jpg);">
+	<div class="s2-page-bnr-con">
+		<h1 class="s2-page-bnr-title">ActionAid Hellas</h1>
+	</div>
+
+	<div class="bnr-circle">
+		<img src="<?php echo THEME_URI;?>/assets/images/NGO-profile-brn-logo-01.png">
+	</div>
+
+</section>
+
+
+<section class="s2-to-be-supported-page-cntlr">
+	<div class="container">
+		<div class="row"> 
+			<div class="col-sm-12">
+				<div class="s2-ngo-profile-block">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+<?php
+get_footer();
+
+?>
